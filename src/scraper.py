e0f42acac7596dd20c94b9d54887930b9ab096c0
Eric <ericscottlagergren@gmail.com>
2014-07-08 10:09:33 -0700

    refactored scraper.py so there's less code and it uses less memory

--- src/scraper.py
+++ src/scraper.py
@@ -13,80 +13,20 @@ from __future__ import division
 import csv
 import datetime
 import subprocess
+import gc
 
 vrdb = 'active.txt'
 
 # Write headings to three output files
 
-with open('ldages.txt', 'wb+') as myfile:
+with open('legdata.txt', 'wb+') as myfile:
     myfile.write('LegDist,AvgAge,NumMales,PerMales,NumFemales,PerFemales' + '\r\n')
 
-with open('cityages.txt', 'wb+') as myfile:
+with open('citydata.txt', 'wb+') as myfile:
     myfile.write('City,AvgAge,NumMales,PerMales,NumFemales,PerFemales' + '\r\n')
 
-with open('precinctages.txt', 'wb+') as myfile:
+with open('precinctdata.txt', 'wb+') as myfile:
     myfile.write('Precinct,AvgAge,NumMales,PerMales,NumFemales,PerFemales' + '\r\n')
-
-
-def legDistrictAges(ld):
-
-	# Opens VRDB and parses it line by line
-
-	reader = csv.DictReader(open(vrdb, 'rb'), delimiter= '\t')
-
-	legdistrict = []
-	ages = []
-	male = []
-	female = []
-
-	for row in reader:
-		if row['LegislativeDistrict'] == str(ld):	
-			legdistrict.append(row)
-
-	# For each person in our specific LD, get their information and append it to the correct lists
-	# 31556952 is used because it's the representation of 365.2425 days in seconds. 365.2425 is used because there's only 97 leap years every 400 years, not 100 (Gregorian calendar)
-
-	for x,value in enumerate(legdistrict):
-		dates = datetime.datetime.now() - datetime.datetime.strptime(value['Birthdate'], '%m/%d/%Y')
-		ages.append(datetime.timedelta.total_seconds(dates) / 31556952)
-		if value['Gender'] == "M":
-			male.append(1)
-		if value['Gender'] == "F":
-			female.append(1)
-
-
-	# This defines the ages. Takes the sum of all the ages and divides it by the total values
-
-	total_values = len(ages)
-	average_age = sum(ages) / total_values
-
-	numfemale = sum(female)
-	nummale = sum(male)
-	perfemale = '{percent:.3%}'.format(percent=numfemale/total_values)
-	permale = '{percent:.3%}'.format(percent=nummale/total_values)
-
-	output = str(ld) + ',' + str(average_age) + ',' + str(nummale) + ',' + str(permale) + ',' + str(perfemale) +',' +  str(numfemale)
-	
-	# Prints output to console so we can see our script is working and with... as myfile appends each lien to with \r\n so we can work with both unix and windows
-
-	print output
-
-	with open('ldages.txt', 'ab+') as myfile:
-	    myfile.write(output + '\r\n')
-
-# In Washington there are 49 LDs, so we spit out each number 1-49 one at a time and feed it to our function that gets the ages
-
-for i in xrange(1, 50):
-	legDistrictAges(i)
-
-
-'''
-
-For the following code is a near duplicate of the first portion, except with values changed because it's formatted for precincts and cities instead of Legislative Districts.
-
-The only real addition would be adding the sorting functions which loop through the file, take all the cities mentioned, add them to an array, pipe that to an external file, strip all formatting, remove duplicates, place all values on their own line, and then reads it as input into a variable that's used to find the ages.
-'''
-
 
 def getCities():
 
@@ -94,71 +34,22 @@ def getCities():
 
 	cities = []
 
+	# Appends all the data from each person inside a specific city
+
 	for row in reader:
 		if row['RegCity']:
 			cities.append(row.get('RegCity'))
 
 	return cities
 
-def sortCities():
-
-	numcities = getCities()
-	numcities = str(numcities)
-
-	with open('citylist.txt', 'ab+') as myfile:
-		myfile.write(numcities)
-
-	subprocess.call(['./shellsubprocess.sh'])
-
-getCities()
-sortCities()
-
-def cityAges(city):
-
-	reader = csv.DictReader(open(vrdb, 'rb'), delimiter='\t')
-
-	cityages = []
-	ages = []
-	male = []
-	female = []
-
-	for row in reader:
-		if row['RegCity'] == city:
-			cityages.append(row)
-
-	for x,value in enumerate(cityages):
-		dates = datetime.datetime.now() - datetime.datetime.strptime(value['Birthdate'], '%m/%d/%Y')
-		ages.append(datetime.timedelta.total_seconds(dates) / 31556952)
-		if value['Gender'] == "M":
-			male.append(1)
-		if value['Gender'] == "F":
-			female.append(1)
-
-	total_values = len(ages)
-	average_age = sum(ages) / total_values
-
-	numfemale = sum(female)
-	nummale = sum(male)
-	perfemale = '{percent:.3%}'.format(percent=numfemale/total_values)
-	permale = '{percent:.3%}'.format(percent=nummale/total_values)
-
-	output = str(city) + ',' + str(average_age) + ',' + str(nummale) + ',' + str(permale) + ',' + str(perfemale) +',' +  str(numfemale)
-
-	print output
-	
-	with open('cityages.txt', 'ab+') as myfile:
-	    myfile.write(output + '\r\n')
-
-listofcities = [line.rstrip() for line in open('citylist.txt')]
-
-for v in listofcities:
-	cityAges(v)
-
 def getPrecincts():
 
 	reader = csv.DictReader(open(vrdb, 'rb'), delimiter='\t')
 
 	precincts = []
+
+	# In Washington counties can use their own precinct codes which are the county code (e.g. King is KI) + precinct code + precinct part
+	# To keep each unique we concat each part with a '+' -- this keeps the values separate but still unique
 
 	for row in reader:
 		if row['PrecinctCode']:
@@ -167,61 +58,95 @@ def getPrecincts():
 	with open('precincts.txt', 'ab+') as myfile:
 	    myfile.write(str(precincts))
 
-def sortPrecincts():
+def sortLists(function, output_file, shell_script):
 
-	numprecincts = getPrecincts()
-	numprecincts = str(numprecincts)
+	numcities = function
+	numcities = str(numcities)
 
-	with open('citylist.txt', 'ab+') as myfile:
-		myfile.write(numprecincts)
+	with open(output_file, 'ab+') as myfile:
+		myfile.write(numcities)
 
-	subprocess.call(['./subshellprecincts.sh'])
+	subprocess.call([shell_script])
 
-getPrecincts()
-sortPrecincts()
+def getInformation(location, identifier, output_file):
 
-def precinctAges(precinct):
+	# Opens VRDB and parses it line by line
 
-	reader = csv.DictReader(open(vrdb, 'rb'), delimiter='\t')
+	reader = csv.DictReader(open(vrdb, 'rb'), delimiter= '\t')
 
-	precinctages = []
+	master_list = []
 	ages = []
-	male = []
-	female = []
+	nummale = 0
+	numfemale = 0
 
-	for row in reader:
-		if str(row['CountyCode']) + '+' + str(row['PrecinctCode']) + '+' + str(row['PrecinctPart']) == precinct:
-			precinctages.append(row)
+	if useprecincts != True:
+		for row in reader:
+			if row[location] == str(identifier):	
+				master_list.append(row)
 
-	for x,value in enumerate(precinctages):
+	if useprecincts:
+		for row in reader:
+			if str(row['CountyCode']) + '+' + str(row['PrecinctCode']) + '+' + str(row['PrecinctPart']) == identifier:
+				master_list.append(row)
+
+	# For each person in our specific LD/city/precinct, get their information and append it to the correct lists
+	# 31556952 is used because it's the representation of 365.2425 days in seconds. 
+	# 365.2425 is used because there's only 97 leap years every 400 years, not 100 (Gregorian calendar)
+	# We also get the number of each gender
+
+	for x,value in enumerate(master_list):
 		dates = datetime.datetime.now() - datetime.datetime.strptime(value['Birthdate'], '%m/%d/%Y')
 		ages.append(datetime.timedelta.total_seconds(dates) / 31556952)
 		if value['Gender'] == "M":
-			male.append(1)
+			nummale += 1
 		if value['Gender'] == "F":
-			female.append(1)
+			numfemale += 1
+
+
+	# This defines the ages. Takes the sum of all the ages and divides it by the total values
+	# It also takes the total number of each gender and divides that by the total
+	# This gives us a percentage with 3 decimal places
 
 	total_values = len(ages)
 	average_age = sum(ages) / total_values
 
-	numfemale = sum(female)
-	nummale = sum(male)
 	perfemale = '{percent:.3%}'.format(percent=numfemale/total_values)
 	permale = '{percent:.3%}'.format(percent=nummale/total_values)
 
-	output = str(precinct) + ',' + str(average_age) + ',' + str(nummale) + ',' + str(permale) + ',' + str(perfemale) +',' +  str(numfemale)
-
-	print output
+	results = str(identifier) + ',' + str(average_age) + ',' + str(nummale) + ',' + str(permale) + ',' + str(numfemale) +',' +  str(perfemale)
 	
-	with open('precinctages.txt', 'ab+') as myfile:
-	    myfile.write(output + '\r\n')
+	# Prints output to console so we can see our script is working
+	# `with... as myfile` appends each line to with \r\n so we can work with both unix and windows
 
+	print results
+
+	with open(output_file, 'ab+') as myfile:
+	    myfile.write(results + '\r\n')
+
+	gc.collect()	
+
+# Gets Legislative District data
+
+for i in xrange(1, 50):
+	getInformation('LegislativeDistrict', i, 'legdata.txt')
+
+# Gets city data
+
+getCities()
+sortLists(getCities, 'citylist.txt', './shellsubprocess.sh')
+listofcities = [line.rstrip() for line in open('citylist.txt')]
+
+for v in listofcities:
+	getInformation('RegCity', v, 'citydata.txt')
+# Gets precinct data
+
+getPrecincts()
+sortLists(getPrecincts, 'precinctlist.txt', './subshellprecincts.sh')
 listofprecincts = [line.rstrip() for line in open('precincts.txt')]
+useprecincts = True
+
+# I'm passing 'a' because getInformation takes 3 args when I only need to pass two
+# I suppose I *could* implement *args, but this works for such a simple scraper script
 
 for y in listofprecincts:
-	precinctAges(y)
-
-
-
-
-
+	getInformation('a', y, 'precinctdata.txt')
