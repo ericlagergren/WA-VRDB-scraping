a48ec83bbeabc46e868454097d020db658ba4a6d
Eric <ericscottlagergren@gmail.com>
2014-08-11 01:03:12 -0700

    This is called testing on your production server lol

--- path/shellsubprocess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-touch citylist.bak;
-cat citylist.txt | sed 's/, /\n/g' > citylist.bak;
-cat citylist.bak | sed "s/'//g" > citylist.txt;
-cat citylist.txt | tr -d '][' > citylist.bak;
-sort citylist.bak | awk '!seen[$0]++' > citylist.txt;
-exit
